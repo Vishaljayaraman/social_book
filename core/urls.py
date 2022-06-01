17d877b10053977a951b2071012ac7c2589ab300
Vishal Jayaraman <jvishal373@outlook.com>
2022-06-01 19:22:58 -0400

    Setup, URL Routing,Template

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+import imp
+from unicodedata import name
+from django import views
+from django.urls import URLPattern, path
+from . import views
+urlpatterns=[
+    path('', views.index, name='index')
+]
